1dec177e327c4254770cd3ee37408fe59d36f6df
Emily Iannizzi <eiannizzi18@gmail.com>
2023-03-31 16:40:16 -0400

    complete

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistant){
+  if (rideDistant <= 400) {
+    return 'This one is on me!';
+  } else if (rideDistant > 400 && rideDistant <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (rideDistant > 2000 && rideDistant <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideDistant > 2500){
+    return 'No can do.';
+  }
+
+  }
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
